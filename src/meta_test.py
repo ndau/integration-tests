20031eafba7b4dfd7f8029aacabb0c9414af2215
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-04-26 12:56:16 +0200

    Update to handle command line options properly.
    contestest.py has to be in the top level to capture custom command
    line flags, so we had to move that.
    
    Tests were asking for symbolic refs, which was fragile: sometimes
    we'd get valid symbolic ref which was not what we were looking for.
    Ensuring we checked out the proper hash makes that more robust.

--- src/meta_test.py
+++ src/meta_test.py
@@ -12,25 +12,34 @@ from src.subp import subp
 @pytest.mark.meta
 def test_chaos_go_repo(chaos_go_repo, request):
     requested_label = request.config.getoption('--chaos-go-label')
+    requested_hash = subp(
+        f'git log {requested_label} -1 --pretty=tformat:"%H"'
+    )
     assert os.path.exists(chaos_go_repo)
     with within(chaos_go_repo):
-        actual_label = subp('git rev-parse --abbrev-ref HEAD')
-        assert requested_label == actual_label
+        actual_hash = subp('git log -1 --pretty=tformat:"%H"')
+        assert requested_hash == actual_hash
 
 
 @pytest.mark.meta
 def test_chaostool_repo(chaostool_repo, request):
     requested_label = request.config.getoption('--chaostool-label')
+    requested_hash = subp(
+        f'git log {requested_label} -1 --pretty=tformat:"%H"'
+    )
     assert os.path.exists(chaostool_repo)
     with within(chaostool_repo):
-        actual_label = subp('git rev-parse --abbrev-ref HEAD')
-        assert requested_label == actual_label
+        actual_hash = subp('git log -1 --pretty=tformat:"%H"')
+        assert requested_hash == actual_hash
 
 
 @pytest.mark.meta
 def test_whitelist_repo(whitelist_repo, request):
     requested_label = request.config.getoption('--whitelist-label')
+    requested_hash = subp(
+        f'git log {requested_label} -1 --pretty=tformat:"%H"'
+    )
     assert os.path.exists(whitelist_repo)
     with within(whitelist_repo):
-        actual_label = subp('git rev-parse --abbrev-ref HEAD')
-        assert requested_label == actual_label
+        actual_hash = subp('git log -1 --pretty=tformat:"%H"')
+        assert requested_hash == actual_hash
