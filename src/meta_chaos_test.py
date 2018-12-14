7938e1ed6d8e71d39da405e114577cc337c4cee6
John Greenland <jsgjnr@yahoo.com>
2018-12-14 14:02:31 -0500

    Updates to support new commands repo and disable more git magic

--- src/meta_chaos_test.py
+++ src/meta_chaos_test.py
@@ -16,25 +16,29 @@ from src.subp import subp
 def test_chaos_go_repo(chaos_go_repo, request):
 #    pdb.set_trace()
     requested_label = request.config.getoption('--chaos-go-label')
-    requested_hash = subp(
-        f'git log {requested_label} -1 --pretty=tformat:"%H"'
-    )
-    assert os.path.exists(chaos_go_repo)
-    with within(chaos_go_repo):
-        actual_hash = subp('git log -1 --pretty=tformat:"%H"')
-        assert requested_hash == actual_hash
+    current_label = subp('git rev-parse --abbrev-ref HEAD')
+    if requested_label == current_label:
+        requested_hash = subp(
+            f'git log {requested_label} -1 --pretty=tformat:"%H"'
+        )
+        assert os.path.exists(chaos_go_repo)
+        with within(chaos_go_repo):
+            actual_hash = subp('git log -1 --pretty=tformat:"%H"')
+            assert requested_hash == actual_hash
 
 
 @pytest.mark.meta
 def test_chaostool_repo(chaostool_repo, request):
     requested_label = request.config.getoption('--chaostool-label')
-    requested_hash = subp(
-        f'git log {requested_label} -1 --pretty=tformat:"%H"'
-    )
-    assert os.path.exists(chaostool_repo)
-    with within(chaostool_repo):
-        actual_hash = subp('git log -1 --pretty=tformat:"%H"')
-        assert requested_hash == actual_hash
+    current_label = subp('git rev-parse --abbrev-ref HEAD')
+    if requested_label == current_label:
+        requested_hash = subp(
+            f'git log {requested_label} -1 --pretty=tformat:"%H"'
+        )
+        assert os.path.exists(chaostool_repo)
+        with within(chaostool_repo):
+            actual_hash = subp('git log -1 --pretty=tformat:"%H"')
+            assert requested_hash == actual_hash
 
 
 @pytest.mark.meta
