fd7866a2e9665f57ee9a231710be51e10309ef09
Eric Schmidt <eric.schmidt@oneiro.io>
2018-12-19 11:06:58 -0800

    Put tool toml file existence checks back in

--- conftest.py
+++ conftest.py
@@ -360,11 +360,16 @@ def chaos_node_and_tool(chaos_node, chaostool_build, chaos_node_exists):
         'PATH': os.environ['PATH'],
     }
 
-    address = 'http://' + chaos_node_exists['address'] + ':' + chaos_node_exists['nodenet0_rpc']
-    subp(
-        f'{chaostool_build["bin"]} conf {address}',
+    conf_path = subp(
+        f'{chaostool_build["bin"]} conf-path',
         env=env,
     )
+    if not os.path.isfile(conf_path):     
+        address = 'http://' + chaos_node_exists['address'] + ':' + chaos_node_exists['nodenet0_rpc']
+        subp(
+            f'{chaostool_build["bin"]} conf {address}',
+            env=env,
+        )
     return {
         'node': chaos_node,
         'tool': chaostool_build,
@@ -386,11 +391,16 @@ def ndau_node_and_tool(ndau_node, ndautool_build, ndau_node_exists):
         'PATH': os.environ['PATH'],
     }
 
-    address = 'http://' + ndau_node_exists['address'] + ':' + ndau_node_exists['nodenet0_rpc']
-    subp(
-        f'{ndautool_build["bin"]} conf {address}',
+    conf_path = subp(
+        f'{ndautool_build["bin"]} conf-path',
         env=env,
     )
+    if not os.path.isfile(conf_path):     
+        address = 'http://' + ndau_node_exists['address'] + ':' + ndau_node_exists['nodenet0_rpc']
+        subp(
+            f'{ndautool_build["bin"]} conf {address}',
+            env=env,
+        )
     return {
         'node': ndau_node,
         'tool': ndautool_build,
