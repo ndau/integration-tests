782de541b77385d1df5e2a929543531f732cb384
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-04-03 14:18:39 +0200

    port block hash test

--- src/ndauapi/block_test.py
+++ src/ndauapi/block_test.py
@@ -1,22 +1,37 @@
 import pytest
 import requests
+from src.util.random_string import random_string
 
 # requests codes aren't technically members of their containing objects
 # pylint: disable=no-member
 
 
-@pytest.fixture
-def min_height(ndauapi):
-    MIN_HEIGHT = 3
+@pytest.fixture(scope="module")
+def current_block(set_up_account, ndauapi):
+    # the hash doesn't get recorded unless there is a tx in it.
+    # simplest way to accomplish that is to set up a random account real quick.
+    set_up_account(random_string("create-block-for-hash"))
     resp = requests.get(f"{ndauapi}/block/current")
     if resp.status_code != requests.codes.ok:
-        pytest.skip(f"failed to get current height; need min {MIN_HEIGHT}")
-    chain_height = resp.json()["block"]["header"]["height"]
+        pytest.skip(f"failed to get current block data")
+    return resp.json()
+
+
+@pytest.fixture(scope="module")
+def min_height(current_block):
+    MIN_HEIGHT = 3
+
+    chain_height = current_block["block"]["header"]["height"]
     if chain_height < MIN_HEIGHT:
         pytest.skip(
             f"this test requires a min block height of {MIN_HEIGHT} "
             f"but current chain height is {chain_height}"
         )
+
+
+@pytest.fixture(scope="module")
+def current_hash(current_block):
+    return current_block["block_meta"]["block_id"]["hash"]
 
 
 @pytest.mark.api
@@ -69,3 +84,30 @@ def test_block_range(ndauapi, min_height, start, end, want_code):
 def test_block_date_range(ndauapi, min_height, start, end, want_code):
     resp = requests.get(f"{ndauapi}/block/daterange/{start}/{end}")
     assert resp.status_code == want_code
+
+
+@pytest.mark.api
+@pytest.mark.parametrize(
+    "send_valid_hash,want_code,want_body",
+    [
+        (None, requests.codes.bad, "blockhash parameter required"),
+        (False, requests.codes.ok, "null"),  # response is empty, so produces null
+        # note: this test currently fails. I don't know why, but I suspect we're
+        # searching for the wrong hash. That said,
+        (True, requests.codes.ok, None),  # should include hash we searched for
+    ],
+)
+def test_block_hash(ndauapi, current_hash, send_valid_hash, want_code, want_body):
+    if send_valid_hash is None:
+        hash = ""
+    elif send_valid_hash:
+        hash = current_hash
+    else:
+        hash = "invalid"
+
+    resp = requests.get(f"{ndauapi}/block/hash/{hash}")
+    print(resp.url)
+    assert resp.status_code == want_code
+    if want_body is None:
+        want_body = current_hash
+    assert want_body in resp.text
