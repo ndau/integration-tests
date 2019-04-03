84550fc7d9242586def7385545f919dcd7797794
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-04-03 15:41:39 +0200

    port get nodelist test

--- src/ndauapi/get_node_test.py
+++ src/ndauapi/get_node_test.py
@@ -15,3 +15,9 @@ def node_id(ndau):
 def test_netinfo(ndauapi, node_id):
     resp = requests.get(f"{ndauapi}/node/{node_id}")
     assert resp.status_code == requests.codes.ok
+
+
+@pytest.mark.api
+def test_node_list(ndauapi):
+    resp = requests.get(f"{ndauapi}/node/nodes")
+    assert resp.status_code == requests.codes.ok
