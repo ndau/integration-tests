8bc4f3855883af4de01731b83a0aa85611c3ba44
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-04-03 12:02:11 +0200

    port block range test

--- src/ndauapi/block_test.py
+++ src/ndauapi/block_test.py
@@ -5,6 +5,20 @@ import requests
 # pylint: disable=no-member
 
 
+@pytest.fixture
+def min_height(ndauapi):
+    MIN_HEIGHT = 3
+    resp = requests.get(f"{ndauapi}/block/current")
+    if resp.status_code != requests.codes.ok:
+        pytest.skip(f"failed to get current height; need min {MIN_HEIGHT}")
+    chain_height = resp.json()["block"]["header"]["height"]
+    if chain_height < MIN_HEIGHT:
+        pytest.skip(
+            f"this test requires a min block height of {MIN_HEIGHT} "
+            f"but current chain height is {chain_height}"
+        )
+
+
 @pytest.mark.api
 @pytest.mark.parametrize(
     "height,want_code",
@@ -23,3 +37,21 @@ def test_block_height(ndauapi, height, want_code):
 def test_block_current_height(ndauapi):
     resp = requests.get(f"{ndauapi}/block/current")
     assert resp.status_code == requests.codes.ok
+
+
+@pytest.mark.api
+@pytest.mark.parametrize(
+    "start,end,want_code",
+    [
+        ("one", 2, requests.codes.bad),  # not a number
+        (0, 2, requests.codes.bad),  # start too low
+        (1, 0, requests.codes.bad),  # end too low
+        (4, 2, requests.codes.bad),  # start > end
+        (1, 102, requests.codes.bad),  # range > 100
+        (3, 3, requests.codes.ok),
+        (1, 2, requests.codes.ok),
+    ],
+)
+def test_block_range(ndauapi, min_height, start, end, want_code):
+    resp = requests.get(f"{ndauapi}/block/range/{start}/{end}")
+    assert resp.status_code == want_code
