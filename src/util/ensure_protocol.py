42fb6bc625ebdb8e329cced6f881e42d458c1392
Ed McNierney <ed@mcnierney.com>
2020-02-07 17:28:20 -0500

    Create/fix copyright messages

--- src/util/ensure_protocol.py
+++ src/util/ensure_protocol.py
@@ -1,3 +1,12 @@
+#  ----- ---- --- -- -
+#  Copyright 2020 The Axiom Foundation. All Rights Reserved.
+# 
+#  Licensed under the Apache License 2.0 (the "License").  You may not use
+#  this file except in compliance with the License.  You can obtain a copy
+#  in the file LICENSE in the source distribution or at
+#  https://www.apache.org/licenses/LICENSE-2.0.txt
+#  - -- --- ---- -----
+
 def ensure_protocol(url, suggestion="http"):
     if "http://" in url and suggestion is "https":
         return f"https://{url.strip('http://')}"
